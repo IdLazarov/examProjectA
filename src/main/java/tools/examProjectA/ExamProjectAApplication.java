5034edcabde8a89d03541fdfec9bcdbc51214416
Ivan <arti244@abv.bg>
2020-05-31 00:14:01 +0300

    Added Rest Controller

--- src/main/java/tools/examProjectA/ExamProjectAApplication.java
+++ src/main/java/tools/examProjectA/ExamProjectAApplication.java
@@ -2,12 +2,23 @@ package tools.examProjectA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ExamProjectAApplication {
+	
+	@GetMapping("/")
+	String Greetings() {
+		return "Hello exam of tools";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamProjectAApplication.class, args);
 	}
 
+	
 }
+
+
