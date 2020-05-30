8efa0840db60c70687f01449b2ffbbf5c22d61c6
Ivan <arti244@abv.bg>
2020-05-31 00:10:50 +0300

    Initial commit

---
+++ src/main/java/tools/examProjectA/ExamProjectAApplication.java
@@ -0,0 +1,13 @@
+package tools.examProjectA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamProjectAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamProjectAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tools/examProjectA/ExamProjectAApplicationTests.java
@@ -0,0 +1,13 @@
+package tools.examProjectA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamProjectAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
